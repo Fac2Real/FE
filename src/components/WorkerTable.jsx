9d583521b0177cfc25d79d111dd7ce35a5373b47
YeIn kim <2076070@ewhain.net>
2025-05-20 17:51:15 +0900

    feat | sprint2 | FRB-152 | 작업자 관리 페이지 제작 | 김예인

---
+++ src/components/WorkerTable.jsx
@@ -0,0 +1,108 @@
+import { useState } from "react";
+
+export default function WorkerTable({ worker_list }) {
+  const [searchType, setSearchType] = useState("byName");
+  const [search, setSearch] = useState("");
+  const [selectedStatus, setSelectedStatus] = useState("전체");
+
+  const filteredWorkers = worker_list.filter((worker) => {
+    if (searchType === "byName") {
+      return worker.name.includes(search.trim());
+    } else if (searchType === "byStatus") {
+      if (selectedStatus === "전체") {
+        return worker_list;
+      }
+      return worker.status === selectedStatus;
+    }
+    return true;
+  });
+  return (
+    <>
+      <div className="table-container">
+        <table className="worker-table">
+          <thead>
+            <tr className="table-search">
+              <th colSpan={6}>
+                <div className="search-container">
+                  <label>
+                    <input
+                      className="radio"
+                      type="radio"
+                      name="searchType"
+                      value="byName"
+                      checked={searchType == "byName"}
+                      onChange={() => setSearchType("byName")}
+                    ></input>
+                    이름으로 검색
+                  </label>
+                  <label>
+                    <input
+                      className="radio"
+                      type="radio"
+                      name="searchType"
+                      value="byStatus"
+                      checked={searchType == "byStatus"}
+                      onChange={() => setSearchType("byStatus")}
+                    ></input>
+                    상태별 분류
+                  </label>
+                  <span className={`search-field-wrapper ${searchType}`}>
+                    {searchType && searchType === "byName" ? (
+                      <div className="search-field fade-in">
+                        <label htmlFor="search">검색어</label>
+                        <input
+                          id="search"
+                          name="search"
+                          value={search}
+                          onChange={(e) => setSearch(e.target.value)}
+                        />
+                      </div>
+                    ) : (
+                      <div className="search-field fade-in">
+                        <select
+                          id="status"
+                          value={selectedStatus}
+                          onChange={(e) => setSelectedStatus(e.target.value)}
+                        >
+                          <option value="전체">전체</option>
+                          <option value="정상">정상</option>
+                          <option value="위험">위험</option>
+                        </select>
+                      </div>
+                    )}
+                  </span>
+                </div>
+              </th>
+            </tr>
+            <tr className="table-header">
+              <th>상태</th> {/* 초록색 빨간색 동그라미 */}
+              <th>직급</th>
+              <th>이름</th>
+              <th>현재 위치</th>
+              <th>웨어러블 ID</th>
+              <th>호출</th>
+            </tr>
+          </thead>
+          <tbody>
+            {filteredWorkers.map((worker, i) => {
+              let tmp = "";
+              if (worker.status == "위험") {
+                tmp = "critical";
+              }
+              return (
+                <tr key={i} className={tmp}>
+                  <td>{worker.status}</td>
+                  <td>{worker.role}</td>
+                  <td>{worker.name}</td>
+                  <td>{worker.zone}</td>
+                  <td>{worker.wearableId}</td>
+                  <td style={{ fontSize: "1.2rem" }}>🚨</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+}
--- src/pages/Safety.jsx
+++ src/pages/Safety.jsx
@@ -1,38 +1,53 @@
+import { useEffect } from "react";
+import axiosInstance from "../api/axiosInstance";
+import WorkerInfoModal from "../components/modal/WorkerInfoModal";
+import WorkerTable from "../components/WorkerTable";
+
 export default function Safety() {
-  const mock_workers = {
-    normal_workers: [],
-    abnormal_workers: [],
-    disconnected_workers: [], // 논의필요!
-  };
+  const mock_workers = [
+    {
+      name: "김00",
+      role: "사원",
+      status: "위험",
+      zone: "포장 구역 A",
+      wearableId: "WEARABLE000111000",
+      email: "test@example.com",
+      phone: "01011112222",
+    },
+    {
+      name: "윤00",
+      role: "공장장",
+      status: "정상",
+      zone: "휴게실",
+      wearableId: "인식되지 않음",
+      email: "test@example.com",
+      phone: "01011112222",
+    },
+    {
+      name: "정00",
+      role: "반장",
+      status: "정상",
+      zone: "조립 구역 B",
+      wearableId: "WEARABLE111111111",
+      email: "test@example.com",
+      phone: "01011112222",
+    },
+  ];
+
+  useEffect(() => {
+    axiosInstance
+      .get("/api/workers")
+      .then(() => {
+        console.log("작업자 정보 get!");
+      })
+      .catch((e) => console.log("작업자 정보 조회 실패", e));
+  }, []);
   return (
     <>
+      <WorkerInfoModal />
       <h1>작업자 안전관리</h1>
-      {/* 정상 작업자 */}
-      <div className="box-wrapper">
-        <div className="top-box" style={{ backgroundColor: "#ebf8fe" }}>
-          정상
-        </div>
-        <div className="bottom-box">
-          {/* 작업자 목록
-           * 아니 뭐더라 */}
-        </div>
-      </div>
-      {/* 이상 작업자 */}
-      <div className="box-wrapper">
-        <div className="top-box" style={{ backgroundColor: "#FFEBCE" }}>
-          주의
-        </div>
-        <div className="bottom-box">
-          {/* 작업자 목록
-           * 아니 뭐더라 */}
-        </div>
-      </div>
-      {/* 연결되지 않은 작업자 */}
-      <div className="box-wrapper">
-        <div className="top-box" style={{ backgroundColor: "#f6f6f6" }}>
-          연결되지 않은 사용자
-        </div>
-        <div className="bottom-box"></div>
+      <div className="monitor-body">
+        <WorkerTable worker_list={mock_workers} />
       </div>
     </>
   );
