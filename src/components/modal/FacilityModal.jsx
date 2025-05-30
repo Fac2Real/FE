44bdf911ffbb53d864990861093432da43f3eea1
YeIn kim <2076070@ewhain.net>
2025-05-30 11:02:49 +0900

    fix | sprint3 | FRB-141 | 매니저 등록 관련 오류 수정 | 김예인

--- src/components/modal/FacilityModal.jsx
+++ src/components/modal/FacilityModal.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import BasicModal from "./BasicModal";
 
-function ModalContents() {
+function ModalContents({ onUpdate }) {
   const [newFacility, setNewFacility] = useState("");
   return (
     <>
@@ -34,31 +34,6 @@ export default function FacilityModal({ isOpen, onClose, zoneInfo, onUpdate }) {
         }}
       />
     );
-    // return (
-    //   <div className="modal-overlay" onClick={onClose}>
-    //     <div className="modal-box" onClick={(e) => e.stopPropagation()}>
-    //       <div onClick={onClose}>
-    //         {/* <XIcon width="1.5rem" height="1.5rem" /> */}
-    //       </div>
-    //       <div className="modal-contents">
-    //         <span>[{zoneInfo}]의 설비 추가</span>
-    // <div className="input-flex">
-    //   <span>설비 이름</span>
-    //   <input
-    //     type="text"
-    //     value={newFacility}
-    //     onChange={(e) => {
-    //       setNewFacility(e.target.value);
-    //     }}
-    //   ></input>
-    // </div>
-    // <div className="button-flex">
-    //   <button onClick={() => onUpdate(newFacility)}>추가</button>
-    // </div>
-    //       </div>
-    //     </div>
-    //   </div>
-    // );
   } else {
     return null;
   }
