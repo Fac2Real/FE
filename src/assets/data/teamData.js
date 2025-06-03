493c4d4b2b1d7aef5c0c72b6ee338dcc12a563cd
YeIn kim <2076070@ewhain.net>
2025-06-04 00:04:57 +0900

    feat | 없음 | 없음 | 팀 소개 페이지 | 김예인
    내용은 미정... 껍데기는 만들었습니다

---
+++ src/assets/data/teamData.js
@@ -0,0 +1,69 @@
+export const teamData = {
+    Factoreal: {
+        content: "임시 파일 내용"
+    },
+    Infra: {
+        content: "인프라 팀 요약",
+        members: [
+            {
+                name: "Kiri",
+                role: "담당한 업무 요약",
+                email: "test@ex.com",
+                github: "githubusername1",
+            },
+            {
+                name: "Subye",
+                role: "담당한 업무 요약",
+                email: "test@ex.com",
+                github: "githubusername1",
+            },
+            {
+                name: "Pero",
+                role: "담당한 업무 요약",
+                email: "test@ex.com",
+                github: "githubusername1",
+            }
+        ]
+    },
+    IoT: {
+        content: "IoT 팀 요약",
+        members: [
+            {
+                name: "Woody",
+                role: "담당한 업무 요약",
+                email: "test@ex.com",
+                github: "githubusername1",
+            },
+            {
+                name: "Stoney",
+                role: "담당한 업무 요약",
+                email: "test@ex.com",
+                github: "githubusername1",
+            },
+        ]
+    },
+    Service: {
+        content: "서비스 팀 요약",
+        members: [
+            {
+                name: "Yeni",
+                role: "담당한 업무 요약",
+                email: "test@ex.com",
+                github: "githubusername1",
+            },
+            {
+                name: "Ddeungee",
+                role: "담당한 업무 요약",
+                email: "test@ex.com",
+                github: "githubusername1",
+            },
+            {
+                name: "Dani",
+                role: "담당한 업무 요약",
+                email: "test@ex.com",
+                github: "githubusername1",
+            }
+        ]
+    }
+
+}
--- src/pages/HiddenPage.jsx
+++ src/pages/HiddenPage.jsx
@@ -1,8 +1,57 @@
+import { useState } from "react";
+import { teamData } from "../assets/data/teamData";
+
 export default function HiddenPage() {
+  const [activeTab, setActiveTab] = useState("Factoreal");
+
   return (
     <>
-      <h1>Easter Egg</h1>
-      <p>우리 팀 정보 넣으면 어떨깝쇼</p>
+      <h1>MEET THE TEAM</h1>
+      <div className="box-wrapper">
+        <div className="top-box team-tab-box">
+          {["Factoreal", "Infra", "IoT", "Service"].map((tab) => (
+            <button
+              key={tab}
+              className={`team-tab-btn ${activeTab === tab ? "active" : ""}`}
+              onClick={() => setActiveTab(tab)}
+            >
+              {activeTab === tab ? "About " : ""}
+              {tab}
+              {tab !== "Factoreal" ? " Team" : ""}
+            </button>
+          ))}
+        </div>
+
+        <div className="bottom-box team-info-box">
+          <p className="team-summary">{teamData[activeTab].content}</p>
+
+          {activeTab !== "Factoreal" && (
+            <div className="members-wrapper">
+              {teamData[activeTab].members.map((m, i) => (
+                <div className="member-card" key={i}>
+                  <img
+                    src={`https://github.com/2076070.png`}
+                    className="member-img"
+                    alt={m.name}
+                  />
+                  <h2 className="member-name">{m.name}</h2>
+                  <div className="member-info-1">{m.role}</div>
+                  <div className="member-info-2">
+                    <strong>EMAIL:</strong> {m.email}
+                  </div>
+                  <div className="member-info-2">
+                    <strong>Github:</strong> {m.github}
+                  </div>
+                </div>
+              ))}
+            </div>
+          )}
+          {activeTab == "Factoreal" && <>
+            <div>임시 로고</div>
+            <div>팀 설명...</div>
+          </>}
+        </div>
+      </div>
     </>
   );
 }
