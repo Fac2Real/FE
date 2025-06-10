8bcdae87f320dfc5212be63bb0d47d82efde381d
YeIn kim <2076070@ewhain.net>
2025-06-10 17:33:45 +0900

    style | sprint3 | 없음 | 실시간 모니터링 페이지 박스 스타일 변경 | 김예인
    - 아이콘이랑 내용 약간씩 삐딱하던 거 맞춤
    - 아이콘 종류 추가!

--- src/components/MonitorIcon.jsx
+++ src/components/MonitorIcon.jsx
@@ -1,24 +1,57 @@
 import TempIcon from "../assets/temp_icon.svg?react";
 import HumidIcon from "../assets/humid_icon.svg?react";
 import CheckedIcon from "../assets/checked_icon.svg?react";
+import GasIcon from "../assets/monitor_icons/gas_icon.svg?react";
+import ElectricIcon from "../assets/monitor_icons/electric_icon.svg?react";
+import PressureIcon from "../assets/monitor_icons/pressure_icon.svg?react";
+import VibIcon from "../assets/monitor_icons/vib_icon.svg?react";
 
 export default function MonitorIcon({
   abnormal_sensor = "온도",
   level,
   color,
 }) {
+  const iconWidth = "25%";
   if (!color) {
     color = level == 0 ? "#1d4a7a" : level == 1 ? "orange" : "red";
   }
 
   if (level == 0) {
-    return <CheckedIcon width="5rem" color={color} opacity="0.5" />;
+    return <CheckedIcon width={iconWidth} color={color} opacity="0.5" />;
   }
 
   if (abnormal_sensor.includes("온도") || abnormal_sensor.includes("temp")) {
-    return <TempIcon width="5rem" color={color} opacity="0.5" />;
+    return <TempIcon width={iconWidth} color={color} opacity="0.5" />;
   }
   if (abnormal_sensor.includes("습도") || abnormal_sensor.includes("humid")) {
-    return <HumidIcon width="5rem" color={color} opacity="0.4" />;
+    return <HumidIcon width={iconWidth} color={color} opacity="0.4" />;
+  }
+  if (
+    abnormal_sensor.includes("진동") ||
+    abnormal_sensor.includes("vibration")
+  ) {
+    return <VibIcon width={iconWidth} color={color} opacity="0.4" />;
+  }
+  if (
+    abnormal_sensor.includes("압력") ||
+    abnormal_sensor.includes("pressure")
+  ) {
+    return <PressureIcon width={iconWidth} color={color} opacity="0.4" />;
+  }
+  if (
+    abnormal_sensor.includes("전력") ||
+    abnormal_sensor.includes("power") ||
+    abnormal_sensor.includes("전류") ||
+    abnormal_sensor.includes("current")
+  ) {
+    return <ElectricIcon width={iconWidth} color={color} opacity="0.4" />;
+  }
+  if (
+    abnormal_sensor.includes("voc") ||
+    abnormal_sensor.includes("휘발성유기화합물") ||
+    abnormal_sensor.includes("dust") ||
+    abnormal_sensor.includes("먼지")
+  ) {
+    return <GasIcon width={iconWidth} color={color} opacity="0.4" />;
   }
 }
