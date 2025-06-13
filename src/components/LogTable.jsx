05db6728202bacdc1c6852c1dd3c494e0d5dcca4
광부입니다 <kwy1379@ajou.ac.kr>
2025-06-13 09:53:08 +0900

    fix | 테스트 | 없음 | Log 테이블 날짜 부분 수정 | 김우영

--- src/components/LogTable.jsx
+++ src/components/LogTable.jsx
@@ -21,9 +21,9 @@ export default function LogTable({ logs, onScrollEnd, scrollBoxRef }) {
               <th style={{ width: "1%" }}>분류</th>
               <th style={{ width: "6%" }}>세분류</th>
               {/* <th style={{ width: "2%" }}>위험도</th> */}
-              <th style={{ width: "3%" }}>발생 시각</th>
               <th style={{ width: "4%" }}>센서ID</th>
               <th style={{ width: "2%" }}>측정값</th>
+              <th style={{ width: "3%" }}>발생 시각</th>
             </tr>
           </thead>
           <tbody>
@@ -39,6 +39,7 @@ export default function LogTable({ logs, onScrollEnd, scrollBoxRef }) {
             )}
             {logs?.map((l, i) => {
               return (
+                
                 <tr
                   key={i}
                   className={
@@ -52,9 +53,9 @@ export default function LogTable({ logs, onScrollEnd, scrollBoxRef }) {
                   <td>{l.targetType}</td>
                   <td>{l.abnormalType}</td>
                   {/* <td>{l.dangerLevel}</td> */}
-                  <td>{l.timestamp}</td>
                   <td>{l.targetId}</td>
                   <td>{l.value}</td>
+                  <td>{new Date(l.timestamp).toLocaleString()}</td>
                 </tr>
               );
             })}
